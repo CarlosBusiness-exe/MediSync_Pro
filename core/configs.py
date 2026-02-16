8f0a6ac66526cfcf4d4402a794b840ab0751e546
CarlosBusiness-exe <business10carlos@gmail.com>
2026-02-16 11:26:40 -0300

    feat: implement database link configuration with isolated data

---
+++ core/configs.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: str = 60 * 24 * 7
+
+    DB_URL: str
+    JWT_SECRET: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=True
+    )
+
+settign: Settings = Settings()
