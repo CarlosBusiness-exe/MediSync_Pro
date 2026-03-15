b56a9d0e79f7caca38304feeaa9ba68a095ba1ff
CarlosBusiness-exe <business10carlos@gmail.com>
2026-03-15 09:45:31 -0300

    feat: implement integration tests for scheduling and conflict validation

---
+++ tests/endpoints/test_appointment.py
@@ -0,0 +1,113 @@
+import pytest
+from fastapi import HTTPException, status
+from sqlmodel import select
+
+from core.configs import settings
+from models.appointment_model import AppointmentModel
+from models.doctor_model import DoctorModel
+from models.patient_model import PatientModel
+
+@pytest.fixture
+def created_doctor(client):
+    doctor = {
+        "name": "Carl", "crm": "017199/GO", "specialty": "cardiologist",
+        "email": "carl@gmail.com", "phone": "+13055335289"
+    }
+    response = client.post(f"{settings.API_V1_STR}/doctors/", json=doctor)
+    return response.json()
+
+@pytest.fixture
+def created_patient(client):
+    patient = {
+        "name": "Jhon", "age": 20, "document_id": "76598712308", "gender": "male",
+        "email": "jhon@gmail.com", "phone": "64982637263", "historical": "diabetic", "address": "Street A"
+    }
+    response = client.post(f"{settings.API_V1_STR}/patients/", json=patient)
+    return response.json()
+
+def test_create_ap_success(client, session, created_doctor, created_patient):
+    appointment = {
+        "ap_date": "2026-03-30",
+        "ap_time": "13:00:00",
+        "doctor_id": created_doctor["id"],
+        "patient_id": created_patient["id"],
+        "status": "Pending"
+    }
+
+    response = client.post(f"{settings.API_V1_STR}/appointments/", json=appointment)
+    data = response.json()
+    assert response.status_code == 201
+
+    assert "id" in data
+    assert "doctor_id" in data
+    assert "patient_id" in data
+
+    assert data["doctor_id"] == created_doctor["id"]
+    assert data["patient_id"] == created_patient["id"]
+
+    query = select(AppointmentModel).where(AppointmentModel.id == data["id"])
+    appointment_in_db = session.exec(query).first()
+    assert appointment_in_db.doctor_id == created_doctor["id"]
+    assert appointment_in_db.patient_id == created_patient["id"]
+
+def test_create_ap_missing(client, created_patient):
+    payload = {
+        "ap_time": "13:00:00",
+        "patient_id": created_patient["id"],
+        "status": "Pending"
+    }
+
+    response = client.post(f"{settings.API_V1_STR}/appointments/", json=payload)
+    assert response.status_code == 422
+
+def test_create_ap_invalid(client, created_patient):
+    appointment = {
+        "ap_date":"2026,03,30",
+        "ap_time":"13,00,00",
+        "doctor_id":"string",
+        "patient_id":created_patient["id"],
+        "status":"Pending"
+    }
+
+    response = client.post(f"{settings.API_V1_STR}/appointments/", json=appointment)
+    assert response.status_code == 422
+
+def test_create_ap_same_time(client, created_doctor, created_patient):
+    appointment = {
+        "ap_date":"2026-03-30",
+        "ap_time":"13:00:00",
+        "doctor_id": created_doctor["id"],
+        "patient_id": created_patient["id"],
+        "status":"Pending"
+    }
+
+    response = client.post(f"{settings.API_V1_STR}/appointments/", json=appointment)
+    data = response.json()
+    assert response.status_code == 201
+
+    same_time_ap = {
+        "ap_date":"2026-03-30",
+        "ap_time":"13:00:00",
+        "doctor_id": created_doctor["id"],
+        "patient_id": created_patient["id"],
+        "status":"Pending"
+    }
+
+    st_response = client.post(f"{settings.API_V1_STR}/appointments/", json=same_time_ap)
+    data = st_response.json()
+
+    assert st_response.status_code == 400
+    assert data["detail"] == "Doctor or Patient already has an appointment scheduled at this time."
+
+def test_get_invalid_id(client):
+    invalid_id = 9999
+
+    response = client.get(f"{settings.API_V1_STR}/appointments/{invalid_id}")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Appointment not found."
+
+def test_get_empty(client):
+    response = client.get(f"{settings.API_V1_STR}/appointments/")
+    assert response.status_code == 200
+    assert response.json() == []
+    assert len(response.json()) == 0
