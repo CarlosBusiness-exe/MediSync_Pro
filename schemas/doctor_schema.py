097ae33796e21576c60d04361d6f35d36c7c44b9
CarlosBusiness-exe <business10carlos@gmail.com>
2026-03-16 10:48:36 -0300

    feat: implement the start and end times for the doctor's work

--- schemas/doctor_schema.py
+++ schemas/doctor_schema.py
@@ -1,4 +1,5 @@
 from typing import List
+from datetime import time
 
 from sqlmodel import SQLModel, Field
 
@@ -10,6 +11,8 @@ class DoctorSchemaBase(SQLModel):
     specialty: str = Field(index=True)
     email: str
     phone: str
+    start_time: time | None = Field(index=True, default=None, nullable=True)
+    end_time: time | None = Field(index=True, default=None, nullable=True)
 
 class DoctorSchemaResponse(DoctorSchemaBase):
     id: int
