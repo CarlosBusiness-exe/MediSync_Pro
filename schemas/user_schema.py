64566f7c841d6a4e1a3f7bc27addab4606e3d341
CarlosBusiness-exe <business10carlos@gmail.com>
2026-03-18 16:26:23 -0300

    feat: add UserRole enum for RBAC

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,10 +1,17 @@
 from typing import Optional
 from sqlmodel import SQLModel
+from enum import Enum
+
+class UserRole(str, Enum):
+    ADMIN = "admin"
+    DOCTOR = "doctor"
+    PATIENT = "patient"
 
 class UserSchemaBase(SQLModel):
     name: str
     email: str
     is_admin: bool
+    role: UserRole
 
 class UserSchemaCreate(UserSchemaBase):
     password: str
