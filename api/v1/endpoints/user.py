89129469854e2ae515bae957a7554b05fdb33ecf
CarlosBusiness-exe <business10carlos@gmail.com>
2026-03-19 16:52:18 -0300

    feat: restrict user management endpoints to admin role

--- api/v1/endpoints/user.py
+++ api/v1/endpoints/user.py
@@ -4,11 +4,14 @@ from sqlmodel import Session
 from typing import List
 
 from core.deps import get_session, get_current_user
-from schemas.user_schema import UserSchemaBase, UserSchemaCreate, UserSchemaResponse
+from schemas.user_schema import UserSchemaBase, UserSchemaCreate, UserSchemaResponse, UserRole
 from models.user_model import UserModel
 from services.user_service import UserService
+from core. authorization import RoleChecker
 
 router = APIRouter()
+
+allow_admin = RoleChecker([UserRole.ADMIN])
 
 @router.get("/logged", response_model=UserSchemaResponse)
 def get_logged(logged_user: UserModel = Depends(get_current_user)):
@@ -23,17 +26,17 @@ def post_user(user_data: UserSchemaCreate, db: Session = Depends(get_session)):
     return UserService.post_user(user_data, db)
 
 @router.get("/", response_model=List[UserSchemaResponse])
-def get_all_users(db: Session = Depends(get_session)):
+def get_all_users(db: Session = Depends(get_session), current_user: UserModel = Depends(allow_admin)):
     return UserService.get_all_users(db)
 
 @router.get("/{user_id}", response_model=UserSchemaResponse)
-def get_user_by_id(user_id: int, db: Session = Depends(get_session)):
+def get_user_by_id(user_id: int, db: Session = Depends(get_session), current_user: UserModel = Depends(allow_admin)):
     return UserService.get_user_by_id(user_id, db)
 
 @router.put("/{user_id}", response_model=UserSchemaResponse)
-def update_user(user_id: int, user_data: UserSchemaCreate, db: Session = Depends(get_session), current_user: UserModel = Depends(get_current_user)):
+def update_user(user_id: int, user_data: UserSchemaCreate, db: Session = Depends(get_session), current_user: UserModel = Depends(allow_admin)):
     return UserService.update_user(user_id, user_data, db)
 
 @router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(user_id: int, db: Session = Depends(get_session), current_user: UserModel = Depends(get_current_user)):
+def delete_user(user_id: int, db: Session = Depends(get_session), current_user: UserModel = Depends(allow_admin)):
     return UserService.delete_user(user_id, db)
